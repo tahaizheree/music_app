2a1a279dbd0a3d62c6113251207e9d5580255c0d
mmateennaveed <mateen.naveed@devsinc.com>
2025-09-16 19:19:27 +0500

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Mateen on 16/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicAppTests/MusicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppTests.swift
+//  MusicAppTests
+//
+//  Created by Mateen on 16/09/2025.
+//
+
+import Testing
+@testable import MusicApp
+
+struct MusicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
